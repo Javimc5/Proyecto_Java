b8f58c35b5d54b23ae4b7ddfc583479676d33528
Iker Solozabal Granados <ikersolozabal4@gmail.com>
2020-02-27 09:12:15 +0100

    Added all

--- src/Ventanas/MenuBusqueda.java
+++ src/Ventanas/MenuBusqueda.java
@@ -28,6 +28,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.ScrollPaneConstants;
 
 public class MenuBusqueda extends JFrame {
 
@@ -112,6 +113,7 @@ public class MenuBusqueda extends JFrame {
 		contentPane.add(btnBuscar);
 		
 		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setBounds(10, 99, 816, 332);
 		contentPane.add(scrollPane);
 		
